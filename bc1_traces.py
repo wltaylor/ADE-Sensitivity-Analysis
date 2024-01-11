8e2451ddfb248af2a038d910c75ed26f5ac0bf91
wltaylor <wltaylor@ucdavis.edu>
2024-01-10 23:54:42 -0800

    1/9/24

---
+++ bc1_traces.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import special
+from SALib.sample import saltelli
+from SALib.analyze import sobol
+
+
+# define the analytical solution for concentration in a type 1 boundary condition environment
+def bc1(Co, L, t, q, p, R):
+    diffusion = 1.0 * 10**-9
+    v = q / p / R
+    alpha = 0.83 * np.log10(L)**2.414
+    Dl = (alpha * v + diffusion) / R
+
+    first_term = special.erfc((L - v * t) / (2 * np.sqrt(Dl * t)))
+    second_term = np.exp(v * L / Dl) * special.erfc((L + v * t) / (2 * np.sqrt(Dl * t)))
+
+    C = (Co / 2) * (first_term + second_term)
+
+    return C
+
+# inputs
+Co = 1000
+L = 25  # meters
+t = np.arange(0, 1000, 1)
+q = 0.056
+p = 0.25
+R = 1.0
+
+test = bc1(Co, L, t, q, p, R)
+
+plt.plot(t, test)
+plt.xlabel('Time')
+plt.ylabel('Concentration')
+plt.title('Concentration vs Time')
+plt.show()
+
+
